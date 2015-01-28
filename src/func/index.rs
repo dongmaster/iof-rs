85eaeba5b5a73b8e36138260cc4d23b2071d8da9
= <=>
2015-01-28 18:23:47 +0100

    Removed unnecessary stuff

--- src/func/index.rs
+++ src/func/index.rs
@@ -11,50 +11,15 @@ pub fn of(haystack: &str, needle: &str) -> uint {
         haystack_vec.push(x);
     }
     
-    
     // New
     for x in needle.chars() {
         needle_vec.push(x);
     }
     
-    
     let haystack_vec_len = haystack_vec.len() - 1;
-    
-    /*
-    // Old
-    for x in range(0, haystack_vec_len) {
-        
-        for y in range(0, needlen) {
-            temp_str.push(lmao[x + y]);
-        }
-        
-        //println!("Needle: {} temp_str: {}", needle, temp_str);
-        
-        if needle == temp_str {
-            println!("Found {} at {}", needle, counter);
-            break;
-        }
-        
-        if x == haystack_vec_len - 4 {
-            break;
-        }
-        
-        counter += 1;
-        temp_str = "".to_string();
-        
-        //temp_str.push_str(lmao[x]);
-        //println!("{}", temp_str);
-    }
-    * */
     
     // New
     for x in range(0, haystack_vec_len) {
-        /*
-        if x == haystack_vec_len - 1 {
-            break;
-        }
-        * */
-        
         // Check if the first letter of the needle is the same as the current haystack letter
         // we fast now
         if needle_vec[0] == haystack_vec[x] {
@@ -70,7 +35,6 @@ pub fn of(haystack: &str, needle: &str) -> uint {
         counter += 1;
         temp_str = "".to_string();
     }
-    
     
     return counter
 }
